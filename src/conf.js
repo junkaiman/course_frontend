1cca5ef9b5cac1207c00b3d11dcba3cb7725712c
Junkai Man <60953254+junkaiman@users.noreply.github.com>
2023-01-10 09:23:30 +0800

    Update conf.js

--- src/conf.js
+++ src/conf.js
@@ -1,9 +1,10 @@
 export default {
     auth_url: "https://forum.gallinula.com/oauth/authorize",
-    client_id: "H3EomPXPCw_Uq96dSKelZUtYHMm0OZZMia6B7flLy7s",
-    scope: "read admin:read:accounts",
-    // redirect_uri: "urn:ietf:wg:oauth:2.0:oob",
-    // redirect_uri: "http://127.0.0.1:3000/login",
-    redirect_uri: "https://course.gallinula.com/login",
+    client_id: "H3EomPXPCw_Uq96dSKelZUtYHMm0OZZMia6B7flLy7s", // prod
+    scope: "read",                                              // prod
+    redirect_uri: "https://course.gallinula.com/login",       // prod
+    // client_id: "JjtdLgzflmgmj8bc6t66DAZE2N7eVzVo5dVeUAD4cDA", // dev
+    // scope: "read",                                            // dev
+    // redirect_uri: "http://127.0.0.1:3000/login",              // dev
     response_type: "code",
 }
